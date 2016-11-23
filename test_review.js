09486b4cbdc40a9540ca075473583c83e5e64086
Corneliousm808 <corneliousmooring@gmail.com>
2016-11-23 08:58:05 -1000

    js test

---
+++ test_review.js
@@ -0,0 +1,45 @@
+// CorneliouS Mooring, 11/23/16, javascript test review
+
+var knight = "Arthur";
+var maiden = "Jane";
+var monster = "Griffin";
+
+weaponChest("Mace", "Sorcerer's Stone", "Exalibur")
+
+hero {
+  wizard = "Merlin"
+  weapon = "Mace"
+  power = 49
+}
+
+swordInTheStone(worthy){
+  if ('Arthur' === 'worthy'){
+    console.log('We have found our King''worthy')
+  }
+  else (){
+    console.log('We must keep searching, for "worthy" is unworthy')
+  }
+}
+
+trueLove(king, queen){
+  if (queen == "Guinever" && king == "Arthur"){
+    console.log('On this day, our ' + 'king' + ' and ' + 'queen ' + 'shall rule happily ever after.')
+  }
+  else ()
+    consloe.log('We must keep searching for true love must reign over Camelot')
+}
+
+trueLove(knight, maiden)
+  if (knight == 'Lacelot')
+
+epicBattle(wizard, enemy, power){
+  if (power <== 15){
+    console.log('Our' + wizard + ' has fallen')
+  }
+  else if (power ==> 16 && <== 40){
+    console.log('our' + 'wizard' + ' has almost slain the enemy')
+  }
+  else (){
+    console.log('Our ' + 'wizard' + ' has slain the enemy!')
+  }
+}
